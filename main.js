45ce860c6244dd539d8bae26327e7b4733f0003d
Santiago Sala <aa2793@student.jamk.fi>
2022-10-27 02:06:52 +0300

    not working

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 const canvas = document.getElementById("myCanvas");
-canvas.width = 400;
+canvas.width = 200;
+
 const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width/2, canvas.width * 0.9);
 
 //Exercise 1: Adding an image size and source (Sorry i used the image from ).
 const image = new Image(60, 45); 
 image.src = "redCar.png"
-
-const car = new Car(image, 200, 200, 30, 50);
+const car = new Car(image, 100, 100, 30, 50);
 
 animate();
 
 function animate(){
   car.update();
   canvas.height = window.innerHeight;
+
+  road.draw(ctx)
+  
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
